4d243286d616c89199df8e27aa70935cec9531c0
Herbeth-OAK <herbeth.pereira@oaktecnologia.com>
2024-07-28 20:38:13 -0300

    edita sh

--- worker.sh
+++ worker.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-
-function do_something {
-  # do something
+# Function to handle signals
+handle_signal() {
+  echo "Caught signal, stopping services..."
+  supervisorctl stop all
   exit 0
 }
 
-trap do_something SIGTERM
+# Trap signals
+trap 'handle_signal' SIGTERM SIGINT
 
+# Start Supervisor
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf &
+
+# Wait for Supervisor to start
 while true; do
   sleep 1
 done
