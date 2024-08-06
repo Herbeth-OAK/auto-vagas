be699ad853b09c26d2070c4c12cbcec899ee23f3
Herbeth-OAK <herbeth.pereira@oaktecnologia.com>
2024-08-06 11:34:13 -0300

    adiciona novo projeto

--- Dockerfile
+++ Dockerfile
@@ -1,65 +1,34 @@
-# Use uma imagem base do Python
+# Use a imagem oficial do Python como base
 FROM python:3.10-slim
 
-# Install dependencies
+# Instala dependências do sistema
 RUN apt-get update && apt-get install -y \
     nginx \
-    wget \
-    unzip \
-    gnupg \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    --no-install-recommends && \
-    rm -rf /var/lib/apt/lists/*
+    supervisor \
+    && rm -rf /var/lib/apt/lists/*
 
-# Install Chrome and ChromeDriver
-RUN apt-get update && apt-get install -y wget gnupg2 \
-    && wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add - \
-    && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
-    && apt-get install -y ./google-chrome-stable_current_amd64.deb \
-    && rm google-chrome-stable_current_amd64.deb \
-    && apt-get install -yqq unzip \
-    && wget -O /tmp/chromedriver.zip https://storage.googleapis.com/chrome-for-testing-public/127.0.6533.72/linux64/chromedriver-linux64.zip \
-    && unzip /tmp/chromedriver.zip chromedriver-linux64/chromedriver -d /usr/local/bin/
-
-
-# Set display port to avoid crash
-ENV DISPLAY=:99
-
-
-# Defina o diretório de trabalho
+# Cria diretórios para o app e configurações
 WORKDIR /app
 
-COPY nginx.conf /etc/nginx/nginx.conf
-
-# Copie os arquivos de requisitos e instale as dependências
+# Copia o arquivo de dependências e instala as dependências do Python
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Copie o restante do código para o contêiner
+# Copia os arquivos do projeto para o container
 COPY . .
 
+# Configura o Nginx
+RUN rm /etc/nginx/sites-enabled/default
+COPY nginx.conf /etc/nginx/nginx.conf
 
-# Instale o Supervisor
-RUN apt-get update && apt-get install -y supervisor
+# Adicione esta linha ao Dockerfile
+COPY session_name.session /app/session_name.session
 
-# Configure o Supervisor
+# Configura o Supervisor
 COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
 
-# Copia o script de entrada
-COPY entrypoint.sh /usr/local/bin/entrypoint.sh
-
-# Adiciona permissões de execução ao script de entrada
-RUN chmod +x /usr/local/bin/entrypoint.sh
-
-RUN mkdir -p /var/log/nginx && chown -R www-data:www-data /var/log/nginx
-
-# Exponha a porta 9001 para o Supervisor
-EXPOSE 9001
+# Exponha a porta do Nginx
 EXPOSE 80
 
-# Defina o comando para iniciar o Supervisor
-#CMD ["/usr/bin/supervisord", "-c", "/etc/supervisor/conf.d/supervisord.conf"]
-
-ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
+# Comando de entrada do Supervisor
+CMD ["supervisord", "-c", "/etc/supervisor/supervisord.conf"]
--- main.py
+++ main.py
@@ -1,116 +1,47 @@
-import time
-import telebot
-import random
-import pyshorteners
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from telegram import Update, Bot
+from telegram.ext import CommandHandler, Application, CallbackContext
+from telethon import TelegramClient
+from datetime import datetime, timedelta, timezone
 
 load_dotenv()
 
-TOKEN = os.getenv('TOKEN')
-CHAT_ID = os.getenv('CHAT_ID')
-bot = telebot.TeleBot(TOKEN)
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+API_ID = os.getenv('API_ID')
+API_HASH = os.getenv('API_HASH')
+PHONE_NUMBER = os.getenv('PHONE_NUMBER')
+CHAT_ID = int(os.getenv('CHAT_ID')) 
 
+bot = Bot(token=TELEGRAM_TOKEN)
+application = Application.builder().token(TELEGRAM_TOKEN).build()
 
-def shorten_url(long_url: str) -> str:
-    try:
-        type_tiny = pyshorteners.Shortener()
-        short_url = type_tiny.tinyurl.short(long_url)
-        return short_url
-    except Exception as e:
-        return long_url
+client = TelegramClient('session_name', API_ID, API_HASH)
 
-def scrape_linkedin_jobs(job_title: str, location: str, pages: int = 1) -> list:
-    pages = pages or 1
+async def get_messages_from_channel():
+    await client.start(PHONE_NUMBER)
+    date_limit = datetime.now(timezone.utc) - timedelta(days=1)
+    messages = []
 
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920x1080")
-
-    driver = webdriver.Chrome(options=options)
-    driver.get(f"https://www.linkedin.com/jobs/search/?keywords={job_title}&location={location}&f_TPR=&f_WT=3%2C2")
-
-    for i in range(pages):
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        try:
-            element = WebDriverWait(driver, 5).until(
-                EC.presence_of_element_located(
-                    (By.XPATH, "//button[contains(text(), 'Show more jobs')]")
-                )
-            )
-            element.click()
-        except Exception:
+    entity = await client.get_entity('@CafeinaVagas')
+    async for message in client.iter_messages(entity):
+        if message.date >= date_limit:
+            messages.append(f'{message.text}')
+        else:
             break
-        time.sleep(random.uniform(3, 7))
 
-    jobs = []
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    job_listings = soup.find_all(
-        "div",
-        class_="base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card",
-    )
+    return messages
 
-    try:
-        for job in job_listings[:10]:
-            job_title = job.find("h3", class_="base-search-card__title").text.strip()
-            job_title = (job_title[:50] + '...') if len(job_title) > 50 else job_title
-            job_company = job.find("h4", class_="base-search-card__subtitle").text.strip()
-            job_location = job.find("span", class_="job-search-card__location").text.strip()
-            apply_link = job.find("a", class_="base-card__full-link")["href"]
-
-            jobs.append(
-                {
-                    "title": job_title,
-                    "company": job_company,
-                    "location": job_location,
-                    "link": shorten_url(apply_link),
-                }
-            )
-    except Exception as e:
-
-        return jobs
-
-    driver.quit()
-    send_jobs_to_telegram(jobs)
-
-def send_jobs_to_telegram(jobs: list) -> None:
-    message = "Vagas de estágios da semana:\n\n"
-    for job in jobs:
-        message += (
-            f"Title: {job['title']}\n"
-            f"Location: {job['location']}\n"
-            f"Link da vaga: {job['link']}\n\n"
-        )
-
-    max_message_length = 4096
-    if len(message) > max_message_length:
-        messages = [message[i:i + max_message_length] for i in range(0, len(message), max_message_length)]
+async def start(update: Update, context: CallbackContext):
+    messages = await get_messages_from_channel()
+    if messages:
+        for msg in messages:
+            await context.bot.send_message(chat_id=CHAT_ID, text=msg)
     else:
-        messages = [message]
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Nenhuma mensagem nas últimas 24 horas.")
 
-    for msg in messages:
-        bot.send_message(CHAT_ID, msg, parse_mode='Markdown')
-        time.sleep(1)
+start_handler = CommandHandler('start', start)
+application.add_handler(start_handler)
 
-# Define command handler for /test
-@bot.message_handler(commands=['test'])
-def send_test_message(message):
-    bot.send_message(CHAT_ID, "Test bem sucedido")
-
-# Define command handler for /start to trigger the scraping manually
-@bot.message_handler(commands=['start'])
-def start_scraping(message):
-    bot.send_message(CHAT_ID, "Iniciando scraping de vagas...")
-    scrape_linkedin_jobs("estagio", "Brazil")
-
-# Start the bot
-bot.polling()
+if __name__ == '__main__':
+    application.run_polling()
